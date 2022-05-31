6847b473711eee94e0c9cfb5d34a483ddec48903
Helbob <millehelbo@outlook.dk>
2022-05-31 09:48:59 +0200

    test

--- normetica/header.php
+++ normetica/header.php
@@ -9,11 +9,14 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head><?php wp_head(); ?></head>
+	<head>
+		<?php wp_head(); ?>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+		<link rel="stylesheet" href="style.css">
+	</head>
 
 	<body <?php body_class(); ?>>
 		<?php
 		wp_body_open();
 
 		do_action( 'woostify_theme_header' );
-	
